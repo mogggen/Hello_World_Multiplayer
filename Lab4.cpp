62be1fa1ed09419892ed7c949f6f8763744d452f
Morgan <melon21@hotmail.se>
2020-10-26 15:13:01 +0100

    Initial Commit

---
+++ Lab4.cpp
@@ -0,0 +1,95 @@
+//Enums och constants
+
+#define MAXNAMELEN  32 
+
+int main()
+{
+    enum ObjectDesc {
+        Human,
+        NonHuman,
+        Vehicle,
+        StaticObject
+    };
+
+    enum ObjectForm {
+        Cube,
+        Sphere,
+        Pyramid,
+        Cone
+    };
+
+    struct Coordinate {
+        int x;
+        int y;
+    };
+
+    enum MsgType {
+        Join,           //Client joining game at server
+        Leave,          //Client leaving game
+        Change,         //Information to clients
+        Event,          //Information from clients to server
+        TextMessage     //Send text messages to one or all
+    };
+    //MESSAGE HEAD, Included first in all messages   
+    struct MsgHead {
+        unsigned int length;     //Total length for whole message   
+        unsigned int seqNo;      //Sequence number
+        unsigned int id;         //Client ID or 0;
+        MsgType type;            //Type of message
+    };
+    //JOIN MESSAGE (CLIENT->SERVER)
+    struct JoinMsg {
+        MsgHead head;
+        ObjectDesc desc;
+        ObjectForm form;
+        char name[MAXNAMELEN];   //null terminated!,or empty
+    };
+    //LEAVE MESSAGE (CLIENT->SERVER)
+    struct LeaveMsg {
+        MsgHead head;
+    };
+    //CHANGE MESSAGE (SERVER->CLIENT)
+    enum ChangeType {
+        NewPlayer,
+        PlayerLeave,
+        NewPlayerPosition
+    };
+    //Included first in all Change messages
+    struct ChangeMsg {
+        MsgHead head;
+        ChangeType type;
+    };
+
+    struct NewPlayerMsg {
+        ChangeMsg msg;          //Change message header with new client id
+        ObjectDesc desc;
+        ObjectForm form;
+        char name[MAXNAMELEN];  //nullterminated!,or empty
+    };
+    struct PlayerLeaveMsg {
+        ChangeMsg msg;          //Change message header with new client id
+    };
+    struct NewPlayerPositionMsg {
+        ChangeMsg msg;          //Change message header
+        Coordinate pos;         //New object position
+        Coordinate dir;         //New object direction
+    };
+    //EVENT MESSAGE (CLIENT->SERVER)
+    enum EventType { Move };
+    //Included first in all Event messages
+    struct EventMsg {
+        MsgHead head;
+        EventType type;
+    };
+    //Variantions of EventMsg
+    struct MoveEvent {
+        EventMsg event;
+        Coordinate pos;         //Newo bject position
+        Coordinate dir;         //New object direction
+    };
+    //TEXT MESSAGE
+    struct TextMessageMsg {
+        MsgHead head;
+        char text[1];   //NULL-terminerad array of chars.
+    };
+}

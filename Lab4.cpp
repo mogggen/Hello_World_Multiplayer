cbd9010344e08510e4aef2ffa26158c48e5a6adb
Morgan <melon21@hotmail.se>
2020-11-06 17:09:53 +0100

    current commit

--- Lab4.cpp
+++ Lab4.cpp
@@ -98,70 +98,8 @@ struct TextMessageMsg {
 	char text[1];   //NULL-terminerad array of chars.
 };
 
-using namespace std;
-
 int x, y;
 bool isFinished = false;
-
-void ReadServer(SOCKET sock)
-{
-	char buf[192];
-	MsgHead* msgHead = (MsgHead*)buf;
-
-	ChangeMsg* changeMsg = (ChangeMsg*)buf;
-	
-	NewPlayerMsg* newPlayerMsg = (NewPlayerMsg*)buf;
-	PlayerLeaveMsg* playerLeaveMsg = (PlayerLeaveMsg*)buf;
-	NewPlayerPositionMsg* newPlayerPositionMsg = (NewPlayerPositionMsg*)buf;
-	int id = -1;
-
-	
-	while (!isFinished)
-	{
-		recv(sock, buf, sizeof(buf), 0);
-		//cout << "\n\n" << "length of Msg: ";
-		/*for (int i = 0; i < 256; i++)
-		{
-			if (buf[i] == -52)
-			{
-				cout << "length: " << i;
-				break;
-			}
-			cout << (int)buf[i] << ";";
-		}
-		cout << "\n\n\n";*/
-		switch (changeMsg->type)
-		{
-		case NewPlayer:
-			cout << '[' << newPlayerMsg->msg.head.seqNo << "]:\t" <<
-				newPlayerMsg->name << " joined\tId=" << newPlayerMsg->msg.head.id << "" << endl;
-			id = newPlayerMsg->msg.head.id;
-			break;
-
-		case PlayerLeave:
-			cout << '[' << playerLeaveMsg->msg.head.seqNo << "]:\tleft server\tId=" << playerLeaveMsg->msg.head.id << endl;
-			break;
-
-		case NewPlayerPosition:
-			cout << '[' << newPlayerPositionMsg->msg.head.seqNo << "]:\tpos:(";
-
-			cout << newPlayerPositionMsg->pos.x << ";";
-			cout << newPlayerPositionMsg->pos.y << ")\tid=" << newPlayerPositionMsg->msg.head.id << endl;
-			if (newPlayerPositionMsg->msg.head.id == id)
-			{
-				x = newPlayerPositionMsg->pos.x;
-				y = newPlayerPositionMsg->pos.y;
-			}
-			break;
-
-		default:
-			cout << "pause debugger..." << endl;
-			cin.get();
-			break;
-		}
-		
-	}
-}
 
 void SendAndReciveToGUI(SOCKET sock)
 {
@@ -190,7 +128,75 @@ void leave(SOCKET sock, int clientid)
 	send(sock, (char*)&leaveMsg, leaveMsg.head.length, 0);
 }
 
+void ReadServer(SOCKET sock)
+{
+	char buf[192];
+	MsgHead* msgHead = (MsgHead*)buf;
 
+	ChangeMsg* changeMsg = (ChangeMsg*)buf;
+	
+	NewPlayerMsg* newPlayerMsg = (NewPlayerMsg*)buf;
+	PlayerLeaveMsg* playerLeaveMsg = (PlayerLeaveMsg*)buf;
+	NewPlayerPositionMsg* newPlayerPositionMsg = (NewPlayerPositionMsg*)buf;
+	int id = -1;
+
+	
+	while (!isFinished)
+	{
+		recv(sock, buf, sizeof(buf), 0);
+		//cout << "\n\n" << "length of Msg: ";
+		/*for (int i = 0; i < 256; i++)
+		{
+			if (buf[i] == -52)
+			{
+				cout << "length: " << i;
+				break;
+			}
+			cout << (int)buf[i] << ";";
+		}
+		cout << "\n\n\n";*/
+		using namespace std;
+		switch (changeMsg->type)
+		{
+		case NewPlayer:
+			cout << '[' << newPlayerMsg->msg.head.seqNo << "]:\t" <<
+				newPlayerMsg->name << " joined\tId=" << newPlayerMsg->msg.head.id << "" << endl;
+			if (id == -1)
+			{
+				id = newPlayerMsg->msg.head.id;
+				SetConsoleTitle((string(newPlayerMsg->name) + string(": id=") + to_string(id)).c_str());
+				move(sock, id); // dummy command to recive the player coordinates
+			}
+			break;
+
+		case PlayerLeave:
+			cout << '[' << playerLeaveMsg->msg.head.seqNo << "]:\tleft server\tId=" << playerLeaveMsg->msg.head.id << endl;
+			break;
+
+		case NewPlayerPosition:
+			cout << '[' << newPlayerPositionMsg->msg.head.seqNo << "]:\tpos:(";
+
+			cout << newPlayerPositionMsg->pos.x << ";";
+			cout << newPlayerPositionMsg->pos.y << ")\tid=" << newPlayerPositionMsg->msg.head.id << endl;
+			if (newPlayerPositionMsg->msg.head.id == id)
+			{
+				x = newPlayerPositionMsg->pos.x;
+				y = newPlayerPositionMsg->pos.y;
+			}
+			break;
+
+		default:
+			cout << "pause debugger..." << endl;
+			cin.get();
+			break;
+		}
+		
+	}
+}
+
+
+
+using namespace std;
 int main()
 {
 	string ipAddress = "130.240.40.7";  // IP Address of the server
@@ -249,8 +255,16 @@ int main()
 	int clientid = msgHead->id;
 	joining.head.id = clientid;
 		
+	// Byt ut mot Non-blocking IO
+	// if (FD_ISSET) är användbart ;)
+	// fd_set
+	// FD_CLR
+	// FD_ZERO
+	// FD_SET
 	thread listen(ReadServer, sock);
 	thread GUI(SendAndReciveToGUI, sock); //coming soon
+	//
+
 	Sleep(1000);
 	// while loop to send data
 	while (!isFinished)
